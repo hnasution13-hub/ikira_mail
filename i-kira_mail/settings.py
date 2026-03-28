3e688f9784ea3f2a172ee2f930b7d4fec8b48188
m4n9.0de <h.nasution13@gmail.com>
2026-03-28 11:02:03 +0700

    CORRECTION1

--- i-kira_mail/settings.py
+++ i-kira_mail/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
         conn_max_age=600,
-        ssl_require=FALSE,
+        ssl_require=False,
     )
 }
 
